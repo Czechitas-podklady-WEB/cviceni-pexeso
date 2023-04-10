c4fed26098bbfaa9b6ac68fdb4f968a1844e3cc1
Filip Chalupa <chalupa.filip@gmail.com>
2023-04-10 17:57:40 +0200

    Řešení

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-console.log('Funguju!')
-
 const karticky = document.querySelectorAll('.karticka')
 
-karticky[0].classList.remove('otocena')
+const otocitKarticku = (event) => {
+	event.target.classList.toggle('otocena')
+}
+
+karticky.forEach((karticka) => {
+	karticka.addEventListener('click', otocitKarticku)
+})
